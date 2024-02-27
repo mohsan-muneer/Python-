7091c9651289655caae61bc4cf148d4cc1081fed
Mohsan muneer <110378665+mohsan-muneer@users.noreply.github.com>
2024-02-27 11:00:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,118 @@
+
+# question1
+print('Break yourself to make yourself')
+#it motivates me to do bigger things and take risk as much as you can
+
+# question2
+
+name="mohsan muneer"
+age=24
+print("the name is :", name, "\n" ,"the age is:", age )
+# Using newline escape sequence
+
+# question3
+
+age= int(input("enter your age:"))
+print(age)
+print(type(age))
+
+# question4
+
+def area_of_rectangle(length,width):
+  area=length*width
+  return area
+
+length=int(input("enter the length:"))
+width=int(input("enter the width:"))
+print(area_of_rectangle(length,width))
+
+# question5
+name="mohsin chohdary"
+age=24
+message="word hard to stay fit and wealthy guys"
+print("my name is {} and my age is {} and my message is {}".format(name,age,message))
+
+# question6
+words="mohsin           chohdary"
+print(words)
+# proper formatting is the need of hour. when you doing software development at large scale, you just dont want to make a headache of yourself and to also find the solution of the problem.  
+
+
+# question7
+number=int(input("enter your number:"))
+print("your input number is:", number)
+multiply_number=number*2
+print("after multiplying it by 2: ", multiply_number)
+
+# question8
+
+
+
+num = int(input("Display multiplication table of: "))
+
+for i in range(1, 4):
+   print(num, 'x', i, '=', num*i)
+
+# question9
+
+number_avg =3,3
+sum=sum(number_avg)
+print(sum)
+length=len(number_avg)
+print(length)
+
+average_of_two_numbers = sum/length
+print("the average of two numbers is:", average_of_two_numbers)
+
+# first of all i have to find the sum of the numbers and then i have to find the length of the numbers and then i have calculated the average of the numbers.
+
+#question10
+
+string1= 'hello how are you doing'
+string2= '  i am doing good'
+cancatenate_string=string1  + string2
+print(cancatenate_string)
+
+
+#question11
+number=float(input("enter your number:"))
+number_int=int(number)
+print(f"your input number is {number_int} ")
+
+#question12
+
+number1=int(input("enter your number:"))
+number2=int(input("enter your number:"))
+number_division=number1/number2 
+rounded_decimal=round(number_division,2)
+print("the divided number is:", rounded_decimal)
+
+
+#question13
+
+message='hello how are you doing \n i am doing good'
+print(message)
+
+# comments
+# we use backslash \n to create a new line in the string.
+
+#question14
+
+number=int(input("enter your number:"))
+number_square=number ** 2
+print("the square number is :" , number_square)
+number_square_root=number ** 0.5
+print("the square root of the number is:", number_square_root)
+
+#question15
+
+weight=input("enter your weight in kg:")
+weight_string_to_int=int(weight)
+print("the weight in kg is:", weight_string_to_int)
+
+
+
+
+
+
+
